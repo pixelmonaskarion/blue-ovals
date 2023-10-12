75d8a34aa056f9ec9797b7016be193e105422226
Ben <69478311+mojo626@users.noreply.github.com>
2023-10-12 09:41:24 -0700

    message sends when enter pressed

--- src/MessagesInput.js
+++ src/MessagesInput.js
@@ -4,8 +4,14 @@ import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import SendIcon from '@mui/icons-material/Send';
 function MessagesInput(props) {
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+          props.onSendClicked();
+        }
+      }
+
     return <div className='inputContainer'>
-        <input type="text" id='messageInput' className='messageInput' placeholder="Type message..." onChange={props.onInputChange} value={props.inputValue}/>
+        <input type="text" id='messageInput' className='messageInput' placeholder="Type message..." onChange={props.onInputChange} onKeyDown={handleKeyDown} value={props.inputValue}/>
         <IconButton onClick={props.onSendClicked} style={{backgroundColor: "#404040", marginRight: "1vw"}}>
             <SendIcon/>
         </IconButton>
