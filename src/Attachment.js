33dde0ec36b2272739d260ff07d0c0015daa9f84
Ben <69478311+mojo626@users.noreply.github.com>
2023-10-19 08:52:31 -0700

    fixed some UI

--- src/Attachment.js
+++ src/Attachment.js
@@ -3,7 +3,7 @@ import Crypto from "./Crypto"
 
 function Attachment(props) {
     if (props.attachment.mimeType.startsWith("image/")) {
-        return <img src={`data:${props.attachment.mimeType};base64,${Crypto.arrayBufferToBase64(props.attachment.data)}`}/>
+        return <img src={`data:${props.attachment.mimeType};base64,${Crypto.arrayBufferToBase64(props.attachment.data)}`} style={{width: '90%', paddingTop: '20px'}}/>
     }
     return <div className='Attachment'>
         <span>{props.attachment.fileName}</span>
